56795b393f0c4b68e0a3e754a94e690fe1d37628
richardson-souza <richardson.allan.souza@gmail.com>
2016-03-07 00:57:32 -0400

    add olamundo.*

---
+++ c++/olamundo/olamundo.cpp
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  olamundo.cpp
+ *
+ *    Description:  Imprime na tela: Ola Mundo!
+ *
+ *        Version:  1.0
+ *        Created:  07/03/2016 00:35:16
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  Misto Duplo Inc.
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+using namespace std;
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  main function
+ * =====================================================================================
+ */
+int main ( int argc, char *argv[] )
+{
+	cout << "Ola, Mundo!";
+	return 0;
+}
---
+++ c/olamundo/olamundo.c
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  olamundo.c
+ *
+ *    Description:  Imprime na tela: Ola, Mundo!
+ *
+ *        Version:  1.0
+ *        Created:  07/03/2016 00:28:41
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  Misto Duplo Inc.
+ *
+ * =====================================================================================
+ */
+
+
+#include <stdio.h> 
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  main function
+ * =====================================================================================
+ */
+int main ( int argc, char *argv[] )
+{
+	printf ("Ola, Mundo!");
+
+	return 0;
+}
